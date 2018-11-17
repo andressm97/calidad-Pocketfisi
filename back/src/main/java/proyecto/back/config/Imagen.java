284a6e75bf930e35e56562e92f69f73c17c9089b
andressm97 <andres.satornicio@gmail.com>
2018-11-17 13:34:08 -0500

    img

--- back/src/main/java/proyecto/back/config/Imagen.java
+++ back/src/main/java/proyecto/back/config/Imagen.java
@@ -3,15 +3,18 @@ package proyecto.back.config;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties.Jdbc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +29,9 @@ import proyecto.back.service.UploadFileService;
 @RestController
 @RequestMapping("/imagen")
 public class Imagen {
+	@Autowired
+	JdbcTemplate JdbcTemplate;
+	
 	
 	@Autowired
 	private UploadFileService service;
@@ -56,17 +62,24 @@ public class Imagen {
 //		
 //	}
 //	
-//	@GetMapping(
-//			  value = "/get2",
-//			  produces = MediaType.IMAGE_JPEG_VALUE
-//			)
-//			public @ResponseBody byte[] getImageWithMediaType() throws IOException {
-//			    InputStream in = getClass()
-//			      .getResourceAsStream("fondo.jpg");
-//			    
-//			    return IOUtils.toByteArray(in);
-//			}
-//	
+	@GetMapping(
+			  value = "/get2",
+			  produces = MediaType.IMAGE_JPEG_VALUE
+			)
+			public @ResponseBody byte[] getImageWithMediaType() throws IOException {
+			    
+			String sql="select img  from news where id_news=1";
+			List<Map<String, Object>> result = JdbcTemplate.queryForList(sql);
+			if (!result.isEmpty()) {
+				
+				byte[] bytes = (byte[]) result.get(0).get("img");
+			    //return IOUtils.toByteArray(in);
+			    return bytes;
+			}
+			
+			return null;
+			}
+	
 	
 	
 	
@@ -84,7 +97,11 @@ public class Imagen {
 		        Long tamano   = file.getSize();
 		        byte[] pixel  = file.getBytes();
 		        
-		        System.out.println("xd :"+pixel.toString());
+		        System.out.println("xdsdasdasda :"+pixel.toString());
+		        
+		        String sql = "UPDATE news  set img=? where id_news =1;";
+		        JdbcTemplate.update(sql,pixel);
+		        
 
 		    }
 
